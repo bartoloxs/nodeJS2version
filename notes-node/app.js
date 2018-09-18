66d0a36fa56dc6a4f6237c5b596884dfb8b0ef5a
Carlos Barrón <carlosjesus.barron@bbva.com>
2018-09-18 15:39:18 +0200

    [NODE-v02] completed notes app

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,11 +5,32 @@ const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
+const bodyOptions = {
+  describe: 'body of note',
+  demand: true,
+  alias: 'b'
+};
+const titleOptions = {
+  describe: 'title of note',
+  demand: true,
+  alias: 't'
+};
 
-const argv = yargs.argv;
-// console.log(argv);
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 const command = argv._[0];
-console.log('Command: ', command);
 
 
 if (command === 'add') {
@@ -17,7 +38,7 @@ if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log('New note registered');
-    notes.logNote(note);    
+    notes.logNote(note);
   } else {
     console.log('Note already registered');
   }
@@ -25,16 +46,15 @@ if (command === 'add') {
 } else if (command === 'list') {
 
   const notesList = notes.getAll();
-
-  notesList.forEach((note) => {
-    notes.logNote(note);
-  })
+  console.log(`Printing ${notes.length}(s)`);
+  notesList.forEach(note => notes.logNote(note));
 
 } else if (command === 'read') {
 
   console.log('Fetching data...');
   const note = notes.getNote(argv.title);
   const message = note ? `Title: ${note.title} Body: ${note.body}` : 'No note was found';
+  notes.logNote(note);
   console.log(message);
 
 } else if (command === 'remove') {
