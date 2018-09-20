a54607be5668567f9c8adc2f749457a96d055916
Carlos Barrón <carlosjesus.barron@bbva.com>
2018-09-20 11:08:12 +0200

    [NODE-V2] wrap functions in promises to chain

--- weather-app/playgrounds/promises.js
+++ weather-app/playgrounds/promises.js
@@ -1,12 +1,30 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Hey, It worked');
-  }, 2500);
-  // reject('Woops... something got wrong :(');
-});
+var asycnAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers.');
+      }
+    }, 1500);
+  });
+};
 
-somePromise.then((message) => {
-  console.log(message);
-}).catch((errMessage) => {
+asycnAdd(1, 5).then((result) => {
+  console.log('Result is ', result);
+}, (errMessage) => {
   console.log(errMessage);
 });
+
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Hey, It worked');
+//   }, 2500);
+//   // reject('Woops... something got wrong :(');
+// });
+
+// somePromise.then((message) => {
+//   console.log(message);
+// }).catch((errMessage) => {
+//   console.log(errMessage);
+// });
